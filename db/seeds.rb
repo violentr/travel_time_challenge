3ac412bc09af33f02bd459a957dc22fafcdf703f
Deniss <den.b@itservicesgroup.co.uk>
2017-07-03 21:25:39 +0100

    Add Extract common functionality to google_maps.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative '../lib/assets/google_maps'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,16 +7,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-type= APPCONFIG["google"]["type"]
-city = APPCONFIG["google"]["city"]
-google_api_key = APPCONFIG["google"]["api_key"]
+extend GoogleMaps
+
 type_time = APPCONFIG["city_mapper"]["attributes"]["type"]
 travel_list = %w(Stratford Moorgate Oval Aldgate Paddington Farringdon Euston Westminster Hoxton Whitechapel)
 
 travel_list.each do |travel_to|
-  area = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/#{type}?address=#{city},#{travel_to}&key=#{google_api_key}")
-  lat = area.parsed_response["results"][0]["geometry"]["location"]["lat"]
-  lng = area.parsed_response["results"][0]["geometry"]["location"]["lng"]
+  response = GoogleMaps.locate_area(travel_to)
+  lat = GoogleMaps.latitude(response)
+  lng = GoogleMaps.longitude(response)
 
   puts "Populating database with travel points, current record is #{travel_to}"
   GeoLocation.create(name: travel_to, latitude: lat, longitude: lng, time_type: type_time)
---
+++ lib/assets/google_maps.rb
@@ -0,0 +1,22 @@
+module GoogleMaps
+
+  def self.locate_area(area)
+    type= APPCONFIG["google"]["type"]
+    city = APPCONFIG["google"]["city"]
+    google_api_key = APPCONFIG["google"]["api_key"]
+    area = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/#{type}?address=#{city},#{area}&key=#{google_api_key}")
+  end
+
+  def self.latitude(response)
+    response.parsed_response["results"][0]["geometry"]["location"]["lat"]
+  end
+
+  def self.longitude(response)
+    response.parsed_response["results"][0]["geometry"]["location"]["lng"]
+  end
+  def self.destination
+    lat = latitude(response)
+    lng = longitude(response)
+    [lat, lng].join(',')
+  end
+end
