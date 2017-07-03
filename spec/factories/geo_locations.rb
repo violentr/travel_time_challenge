d240da79598ce5c9a193ad0c64c864e82e08d754
Deniss <den.b@itservicesgroup.co.uk>
2017-07-03 12:00:36 +0100

    Add Rspec config, make sure all necessary files loaded

---
+++ spec/factories/geo_locations.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :geo_location do
+   name "Stratford"
+   latitude "51.04"
+   longitude "-0.34"
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require_relative '../app/services/negotiator_trip_service'
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -6,7 +13,7 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
+  config.include FactoryGirl::Syntax::Methods
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
 end
