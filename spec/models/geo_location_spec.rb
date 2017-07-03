7d04aaea2a8940ed38fafbd4162be8d2d9551120
Deniss <den.b@itservicesgroup.co.uk>
2017-07-03 23:48:58 +0100

    Add Spec for .create_if_not_found

--- spec/models/geo_location_spec.rb
+++ spec/models/geo_location_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GeoLocation, type: :model do
+  let(:location) { create(:geo_location) }
   it {is_expected.to respond_to(:latitude) }
   it {is_expected.to respond_to(:longitude) }
   it {is_expected.to respond_to(:time_type) }
   it {is_expected.to respond_to(:name) }
   it {is_expected.to respond_to(:travel_time_minutes) }
+
+  it "should create record if not found " do
+    expect(GeoLocation.create_if_not_found(location)).to eq(location)
+  end
 end
